aa384b94d5f7f4699b4f3d5d02f477a2a283399e
Sildian <n.mayerweiler@gmail.com>
2025-04-12 20:55:47 +0200

    Create EditSceneScreen

--- uiLayer/src/main/java/com/sildian/apps/storywriter/uilayer/scene/EditSceneScreen.kt
+++ uiLayer/src/main/java/com/sildian/apps/storywriter/uilayer/scene/EditSceneScreen.kt
@@ -1,19 +1,66 @@
 package com.sildian.apps.storywriter.uilayer.scene
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sildian.apps.storywriter.designsystem.theme.StoryWriterTheme
+import com.sildian.apps.storywriter.uilayer.R
 
 @Composable
 internal fun EditSceneScreen() {
-    Scaffold { contentPadding ->
-        Text(
-            modifier = Modifier.padding(paddingValues = contentPadding),
-            text = "Edit Scene Screen",
+    Scaffold(
+        contentWindowInsets = WindowInsets.safeDrawing,
+        bottomBar = {
+            BottomAppBar(
+                contentPadding = PaddingValues(16.dp),
+                windowInsets = BottomAppBarDefaults.windowInsets.union(WindowInsets.ime),
+            ) {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { /*TODO*/ },
+                ) {
+                    Text(text = stringResource(id = R.string.edit_scene_button_save))
+                }
+            }
+        }
+    ) { contentPadding ->
+        var sceneDescription by rememberSaveable { mutableStateOf("") }
+        TextField(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(contentPadding)
+                .padding(16.dp),
+            value = sceneDescription,
+            onValueChange = { sceneDescription = it },
+            label = {
+                Text(text = stringResource(id = R.string.edit_scene_label_scene))
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.edit_scene_placeholder_scene))
+            },
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
         )
     }
 }
