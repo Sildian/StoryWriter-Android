f4848367085f2f84a46d4a883cc6eb721dec7d45
Sildian <n.mayerweiler@gmail.com>
2025-08-10 20:24:44 +0200

    Create SceneItem Composable

---
+++ uiLayer/src/main/java/com/sildian/apps/storywriter/uilayer/scene/showscenes/SceneItem.kt
@@ -0,0 +1,48 @@
+package com.sildian.apps.storywriter.uilayer.scene.showscenes
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sildian.apps.storywriter.designsystem.theme.StoryWriterTheme
+import com.sildian.apps.storywriter.uilayer.scene.SceneUi
+
+@Composable
+internal fun SceneItem(
+    scene: SceneUi,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        modifier = modifier
+            .background(
+                color = MaterialTheme.colorScheme.secondaryContainer,
+                shape = MaterialTheme.shapes.large.copy(
+                    topEnd = ZeroCornerSize,
+                    bottomStart = ZeroCornerSize,
+                )
+            ).padding(16.dp),
+        text = scene.description,
+        style = MaterialTheme.typography.bodyLarge,
+        color = MaterialTheme.colorScheme.onSecondaryContainer,
+        maxLines = 3,
+        overflow = TextOverflow.Ellipsis,
+    )
+}
+
+@Preview
+@Composable
+private fun SceneItemPreview() {
+    StoryWriterTheme {
+        SceneItem(
+            scene = SceneUi(
+                description = "Il était une fois un chevalier qui voulait séduire la princesse, mais le château dans lequel elle vivait était bien trop grand et il ne la trouvait pas. Il se demandait si combattre un dragon ne l'aiderait pas dans sa quête, bien qu'il n'y ait pas un grand rapport."
+            ),
+        )
+    }
+}
