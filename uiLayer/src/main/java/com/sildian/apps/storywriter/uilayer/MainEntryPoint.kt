edd80e72af0f05727724456999a0d2950b949d13
Sildian <n.mayerweiler@gmail.com>
2025-08-16 18:23:16 +0200

    Wrap ShowScenes and EditScene screens within a ListDetailPaneScaffold

--- uiLayer/src/main/java/com/sildian/apps/storywriter/uilayer/MainEntryPoint.kt
+++ uiLayer/src/main/java/com/sildian/apps/storywriter/uilayer/MainEntryPoint.kt
@@ -1,9 +1,28 @@
 package com.sildian.apps.storywriter.uilayer
 
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.navigation.NavigableListDetailPaneScaffold
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
 import com.sildian.apps.storywriter.uilayer.scene.editscene.EditSceneEntryPoint
+import com.sildian.apps.storywriter.uilayer.scene.showscenes.ShowScenesEntryPoint
 
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
 @Composable
 fun MainEntryPoint() {
-    EditSceneEntryPoint()
+    val scaffoldNavigator = rememberListDetailPaneScaffoldNavigator<Any>()
+    NavigableListDetailPaneScaffold(
+        navigator = scaffoldNavigator,
+        listPane = {
+            AnimatedPane {
+                ShowScenesEntryPoint()
+            }
+        },
+        detailPane = {
+            AnimatedPane {
+                EditSceneEntryPoint()
+            }
+        }
+    )
 }
